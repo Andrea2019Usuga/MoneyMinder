3eeaaf75fa7cf0dbc123c3a7ea874acddc16cbcc
YeimiBecerra <Yeimybecerraibarra@gmail.com>
2024-10-22 16:13:35 -0500

    organizo boton de restablecer contraseña, redireccion de inicio sesion en la vista del anuncio restablecer contraseña, se agrego espacio lineal horizontal entre la barra de busqueda y el bton de agregar ingreso, gasto, metas de ahorro, redirrecion del boton de configuracion desde la vista metas de ahorro

--- app/Views/anuncioRestablecerContrasena.php
+++ app/Views/anuncioRestablecerContrasena.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anuncio Restablecer Contraseña - Money Minder</title>
     <link rel="stylesheet" href="/MoneyMinder/public/css/anuncioRestablecerContrasena.css">
-    <script src="js/5anuncio restablecer contraseña.js" defer></script>
 </head>
 <body>
     <main>
@@ -13,16 +12,16 @@
         <img src="/MoneyMinder/public/img/logo.jpeg" alt="Money Minder Logo" class="logo-login">
             <div>
                 <h1>Notificación restablecer contraseña</h1>
-                <p class="background-paragraph">Verifica la bandeja de entrada de tu <br> correo electrónico para obtener el <br> código de restablecimiento.</p>
+                <p class="background-paragraph">Verifica la bandeja de entrada de tu <br> correo electrónico para obtener la <br> contraseña de restablecimiento.</p>
                 
                 <div class="button-container">
-                    <a href="2Inicio sesion.html" class="button-link">
+                    <a href="/MoneyMinder/index.php/inicioSesion" class="button-link">
                         <button type="button">Regresar a inicio de sesión</button>
                     </a>
                 </div>
             </div>
                     
-                <form action="2Inicio sesion.html" method="post">
+                <form action="/MoneyMinder/index.php/inicioSesion" method="post">
                     
                 </form>
             </div>
